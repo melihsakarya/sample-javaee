df6dd92162ab41b2048eac27822887185f08d99b
melih <melih@192.168.2.194>
2017-02-19 19:21:07 +0200

    Unit test detail

--- src/test/java/com/saha/tdd/unit/service/test/UserServiceUnitTest.java
+++ src/test/java/com/saha/tdd/unit/service/test/UserServiceUnitTest.java
@@ -1,11 +1,29 @@
 package com.saha.tdd.unit.service.test;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import com.saha.tdd.crm.dao.UserDao;
+import com.saha.tdd.crm.entity.User;
+import com.saha.tdd.crm.service.UserService;
+
+@RunWith(MockitoJUnitRunner.class)
 public class UserServiceUnitTest {
 
+	
+	@InjectMocks UserService userService;
+	@Mock UserDao userDao;
+	
 	@Test
 	public void saveUserTest(){
-		System.out.println("sample");
+		User user = new User();
+		user.setName("Melih");
+		user.setLastname("Sakarya");
+		user.setAge(35);
+		Assert.assertTrue(userService.save(user));
 	}
 }
